517dfc23c9c890c5f9ad34eedcf4efe001e65946
Beshalaby <68404054+Beshalaby@users.noreply.github.com>
2021-03-09 09:00:43 -0500

    Add files via upload

---
+++ autoZoom/background.js
@@ -0,0 +1,60 @@
+var today = new Date;
+var time = today.getUTCHours() + ":" + today.getUTCMinutes()
+console.log(time);
+var submitButton = document.getElementById("submitButton").
+
+submitButton.addEventListener("click", submitLinks())
+
+function submitLinks() {
+    var enteredLinks = document.getElementById("link1");
+    console.log("hello");
+}
+
+var isTrue=false
+if (time == "13:45" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/91435985446?pwd=V25CQzBBaUJUWGd5ZUR1cFk0ajA0dz09","_blank");
+    isTrue=true;
+
+}
+var isTrue=false
+if (time == "14:37" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/91606745556?pwd=RWlwVDkrTW9VQ2ZiUkRDMlM2UytOUT09","_blank");
+    isTrue=true;
+}
+
+var isTrue=false
+if (time == "15:29" && isTrue==false) {
+    window.open("https://zoom.us/j/99850648414?pwd=OUROL0s3QkQ2VmlCKzMrMWQ2blNBZz09","_blank");
+    isTrue=true;
+
+}
+var isTrue=false
+if (time == "16:21" && isTrue==false) {
+    window.open("https://zoom.us/j/94561200668?pwd=c3dDOUlNOWRXTUpGY29zUmdSL240UT09","_blank");
+    isTrue=true;
+}
+
+var isTrue=false
+if (time == "18:1" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/92771305909?pwd=ZnpiNTVkK1F2VHV6eXVMY0drd05tUT09","_blank");
+    isTrue=true;
+}
+
+var isTrue=false
+if (time == "18:53" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/96994898605?pwd=cGlLSitYRld1aDJkejl2alNhckFIdz09","_blank");
+    isTrue=true;
+}
+
+var isTrue=false
+if (time == "18:31" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/96994898605?pwd=cGlLSitYRld1aDJkejl2alNhckFIdz09","_blank");
+    isTrue=true;
+}
+
+var isTrue=false
+if (time == "18:32" && isTrue==false) {
+    window.open("https://wcpsmd.zoom.us/j/96994898605?pwd=cGlLSitYRld1aDJkejl2alNhckFIdz09","_blank");
+    isTrue=true;
+}
+
